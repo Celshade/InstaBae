eebf3a0a7f44a16316d8832cfd3cfc7d723c8e93
Danny Collins | Celshade <ggcelshade@gmail.com>
2018-11-01 12:25:36 -0400

    Handle infinite scroll -> bottom of page.

--- instabae/instabae.py
+++ instabae/instabae.py
@@ -10,7 +10,7 @@ BAE = input("Enter bae's username: ")
 # TODO Add 'modes' ('S' for Spectator mode | 'N' for Ninja mode)
 
 # Useful constants.
-GECKO = ".//drivers/geckodriver.exe"
+GECKO = "../drivers/geckodriver.exe"
 GRAM = "https://www.instagram.com/accounts/login/?source=auth_switcher"
 TARGET = f"https://www.instagram.com/{BAE}/"
 ME = f"https://www.instagram.com/{USER}/"
@@ -39,10 +39,24 @@ try:
     # Find the total amount of pictures
     total = driver.find_element_by_class_name("g47SY ")
     total = total.get_attribute("textContent")
+    # Scroll to bottom of page
+    height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        # Scroll to current bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        sleep(0.5)
+
+        # Compare height vs new_height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == height:
+            break
+        else:
+            height = new_height
+
     # TODO Find the total amount of pictures liked
     # TODO Like un-liked pictures
     # TODO Like each un-liked pictures
-
+    sleep(1)
 except Exception:
     print("Something went wrong")
 finally:
