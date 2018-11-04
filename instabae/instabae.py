1002dcb65b80e5d70601df52c16cb91bbe5275c3
Danny Collins | Celshade <ggcelshade@gmail.com>
2018-11-04 15:41:25 -0500

    Trim Xpaths and fix scrolling issue.

--- instabae/instabae.py
+++ instabae/instabae.py
@@ -15,9 +15,9 @@ GRAM = "https://www.instagram.com/accounts/login/?source=auth_switcher"
 TARGET = f"https://www.instagram.com/{BAE}/"
 ME = f"https://www.instagram.com/{USER}/"
 
-# TODO Add additional driver support for IE and Chrome.
 # TODO def driver(browser: str="FireFox") # select necessary driver
 driver = webdriver.Firefox(executable_path=GECKO)
+# TODO Add additional driver support for IE and Chrome.
 
 # ### Open browser, log in, navigate to target. ### #
 driver.get(GRAM)
@@ -35,39 +35,46 @@ driver.get(TARGET)
 sleep(3)
 # TODO Handle TARGET not being found (incorrect username)
 
-try:
-    # Find the total amount of pictures
-    total = driver.find_element_by_class_name("g47SY ")
-    total = total.get_attribute("textContent")
-    # Scroll to bottom of page
-    height = driver.execute_script("return document.body.scrollHeight")
-    while True:
-        # Scroll to current bottom
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        sleep(0.5)
+# Find the total number of pictures
+_ = driver.find_element_by_class_name("g47SY ")
+total = _.get_attribute("textContent")
 
-        # Compare height vs new_height
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == height:
+try:
+    # TODO Find the total number of 'liked' pictures
+    # Get current page height
+
+    # Scroll to bottom of page
+    while True:
+        height = driver.execute_script("return document.body.scrollHeight")
+        print(f"0riginal: {height}")
+        # Scroll to current bottom
+        scroll = "window.scrollTo(0, document.body.scrollHeight);"
+        driver.execute_script(scroll)
+        sleep(1)
+
+        # Compare height values
+        current = driver.execute_script("return document.body.scrollHeight")
+        print(f"0riginal: {current}\n------------")
+        if current == height:
             break
         else:
-            height = new_height
+            height = current
+    sleep(1)
 
-    # TODO Find the total amount of pictures liked
     # TODO Like un-liked pictures
     # TODO Like each un-liked pictures
-    sleep(1)
 except Exception:
     print("Something went wrong")
 finally:
     # ### Logging out and closing the browser ### #
+    # Nav to USER's profile
     driver.get(ME)
     sleep(3)
-
-    settings = "/html/body/span/section/main/div/header/section/div[1]/div/button"
+    # Find settings
+    settings = "//button[@class='_0mzm- dCJp8']"
     driver.find_element_by_xpath(settings).click()
-
-    loggout = "/html/body/div[3]/div/div/div/div/button[6]"
+    # Loggout and close browser.
+    loggout = "//button[text()='Log Out']"
     driver.find_element_by_xpath(loggout).click()
     sleep(3)
     driver.quit()
