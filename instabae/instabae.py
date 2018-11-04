9afdee3ec8268fcf6b6081fd2ce95e063bb3f7b0
Danny Collins | Celshade <ggcelshade@gmail.com>
2018-11-04 16:08:09 -0500

    Add scroll() and loggout() functions.

--- instabae/instabae.py
+++ instabae/instabae.py
@@ -3,78 +3,89 @@ from time import sleep
 
 from selenium import webdriver
 
-# Set user/password/target values prior to opening browser
+# Useful constants.
 USER = input("Enter username: ")
 PASSWORD = input("Enter password: ")
 BAE = input("Enter bae's username: ")
-# TODO Add 'modes' ('S' for Spectator mode | 'N' for Ninja mode)
-
-# Useful constants.
 GECKO = "../drivers/geckodriver.exe"
 GRAM = "https://www.instagram.com/accounts/login/?source=auth_switcher"
 TARGET = f"https://www.instagram.com/{BAE}/"
-ME = f"https://www.instagram.com/{USER}/"
-
-# TODO def driver(browser: str="FireFox") # select necessary driver
-driver = webdriver.Firefox(executable_path=GECKO)
+HOME = f"https://www.instagram.com/{USER}/"
+DRIVER = webdriver.Firefox(executable_path=GECKO)
 # TODO Add additional driver support for IE and Chrome.
+# TODO Add function to select driver [browser] of choice
+# TODO Add 'modes' ('S' for Spectator mode | 'N' for Ninja mode)
 
-# ### Open browser, log in, navigate to target. ### #
-driver.get(GRAM)
-sleep(3)
 
-username = driver.find_element_by_name("username")
-password = driver.find_element_by_name("password")
-username.send_keys(USER)
-password.send_keys(PASSWORD)
-password.submit()
-sleep(3)
-# TODO Handle incorrect user/password
+def scroll(speed: float=1.00) -> None:
+    """Scroll to the bottom of a page.
 
-driver.get(TARGET)
-sleep(3)
-# TODO Handle TARGET not being found (incorrect username)
-
-# Find the total number of pictures
-_ = driver.find_element_by_class_name("g47SY ")
-total = _.get_attribute("textContent")
-
-try:
-    # TODO Find the total number of 'liked' pictures
-    # Get current page height
-
-    # Scroll to bottom of page
+    Args:
+        speed: Seconds to sleep after each scroll (default=1.00).
+    """
     while True:
-        height = driver.execute_script("return document.body.scrollHeight")
-        print(f"0riginal: {height}")
-        # Scroll to current bottom
+        # Get current page height
+        height = DRIVER.execute_script("return document.body.scrollHeight")
+        # Scroll to the bottom of the page
         scroll = "window.scrollTo(0, document.body.scrollHeight);"
-        driver.execute_script(scroll)
-        sleep(1)
+        DRIVER.execute_script(scroll)
+        sleep(speed)  # For refreshing
 
         # Compare height values
-        current = driver.execute_script("return document.body.scrollHeight")
-        print(f"0riginal: {current}\n------------")
+        current = DRIVER.execute_script("return document.body.scrollHeight")
         if current == height:
             break
         else:
             height = current
     sleep(1)
 
-    # TODO Like un-liked pictures
-    # TODO Like each un-liked pictures
-except Exception:
-    print("Something went wrong")
-finally:
-    # ### Logging out and closing the browser ### #
+
+def loggout(speed: float=3.00) -> None:
+    """Loggout and close the browser.
+
+    Args:
+        speed: Seconds to sleep prior to accessing page 'settings' (default=3).
+    """
     # Nav to USER's profile
-    driver.get(ME)
+    DRIVER.get(HOME)
     sleep(3)
     # Find settings
     settings = "//button[@class='_0mzm- dCJp8']"
-    driver.find_element_by_xpath(settings).click()
+    DRIVER.find_element_by_xpath(settings).click()
     # Loggout and close browser.
     loggout = "//button[text()='Log Out']"
-    driver.find_element_by_xpath(loggout).click()
+    DRIVER.find_element_by_xpath(loggout).click()
     sleep(3)
-    driver.quit()
+    DRIVER.quit()
+
+# ### Open browser, log in, navigate to target. ### #
+DRIVER.get(GRAM)
+sleep(3)
+
+username = DRIVER.find_element_by_name("username")
+password = DRIVER.find_element_by_name("password")
+username.send_keys(USER)
+password.send_keys(PASSWORD)
+password.submit()
+sleep(3)
+# TODO Handle incorrect user/password
+
+DRIVER.get(TARGET)
+sleep(3)
+# TODO Handle TARGET not being found (incorrect username)
+
+# Find the total number of pictures
+_ = DRIVER.find_element_by_class_name("g47SY ")
+total = _.get_attribute("textContent")
+
+# TODO Find the total number of 'liked' pictures
+# Scroll to bottom of page
+scroll()
+try:
+    # TODO Like un-liked pictures
+    # TODO Like each un-liked pictures
+    pass
+except Exception:
+    print("Something went wrong")
+finally:
+    loggout()
