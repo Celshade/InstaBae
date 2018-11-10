cce17f324cfb3041eb9b5b927fd4b23c97faeab5
Danny 'Celshade' Collins <ggcelshade@gmail.com>
2018-11-10 14:38:33 -0500

    Encapsulate into a class.

--- instabae/instabae.py
+++ instabae/instabae.py
@@ -1,101 +1,146 @@
-"""Auto like your [wo]man's pictures on instagram."""
+"""Auto like your [wo]man's pictures on instagram.
+
+Classes:
+    BaeFinder(object): Handle communication with Instagram.
+Functions:
+    # TODO
+Attributes:
+    # TODO
+"""
 from time import sleep
 
 from selenium import webdriver
 
-# Useful constants.
-USER = input("Enter username: ")
-PASSWORD = input("Enter password: ")
-BAE = input("Enter bae's username: ")
+# Drivers
 GECKO = "../drivers/geckodriver.exe"
-GRAM = "https://www.instagram.com/accounts/login/?source=auth_switcher"
-TARGET = f"https://www.instagram.com/{BAE}/"
-HOME = f"https://www.instagram.com/{USER}/"
-DRIVER = webdriver.Firefox(executable_path=GECKO)
-# TODO Add additional driver support for IE and Chrome.
-# TODO Add function to select driver [browser] of choice
-# TODO Add 'modes' ('S' for Spectator mode | 'N' for Ninja mode)
 
 
-def scroll_and_grab(speed: float=2.00) -> set:
-    """Scroll to the bottom of a page and return a set of all picture hrefs.
+class BaeFinder(object):
+    """Search Instagram for BAE and like their un-liked pictures.
 
-    Args:
-        speed: Seconds to sleep after each scroll (default=1.00).
+    Attributes:
+        # TODO
+        TARGET: The username of your target.
+    Public Methods:
+        log_in(): Log into Instagram.
+        scroll_and_grab(): Scroll through the page and gather post info.
+        log_out(): Log out of Instagram and close the browser session.
     """
-    links = set()  # Automatically filter duplicates
 
-    while True:
-        # Add all currently 'visible' posts to the list
-        path = "//div[@class='Nnq7C weEfm']//descendant::a"
-        for x in DRIVER.find_elements_by_xpath(path):
-            links.add(x.get_attribute('href'))
+    def __init__(self) -> None:
+        self.BAE = input("Enter the username of bae: ")
+        self.USER = input("Enter your username: ")
+        self._PASSWORD = input("Enter your password: ")
+        self.DRIVER = webdriver.Firefox(executable_path=GECKO)
+        # TODO Add additional driver support for IE and Chrome.
+        # TODO Add function to select driver [browser] of choice
+        # TODO Add 'modes' ('S' for Spectator mode | 'N' for Ninja mode)
 
-        # Get current page height and scroll to the bottom of the page
-        height = DRIVER.execute_script("return document.body.scrollHeight")
-        j_scroll = "window.scrollTo(0, document.body.scrollHeight);"
-        DRIVER.execute_script(j_scroll)
-        sleep(speed)  # Allow refreshing
+    def __repr__(self) -> str:
+        """Return basic information carried by BaeFinder."""
+        pass
 
-        # Compare height values
-        current = DRIVER.execute_script("return document.body.scrollHeight")
-        if current == height:
-            break
-        else:
-            height = current
-    sleep(1)
-    return links
+    def log_in(self, speed: float=3.00) -> None:
+        """Open browser, log into Instagram, and navigate to target.
+
+        Args:
+            speed: Seconds to sleep (refresh) between pages.
+        """
+        GRAM = "https://www.instagram.com/accounts/login/?source=auth_switcher"
+        TARGET = f"https://www.instagram.com/{self.BAE}/"
+
+        # Open browser -> Instagram
+        self.DRIVER.get(GRAM)
+        sleep(3)
+
+        # Log in
+        username = self.DRIVER.find_element_by_name("username")
+        password = self.DRIVER.find_element_by_name("password")
+        username.send_keys(self.USER)
+        password.send_keys(self._PASSWORD)
+        del self._PASSWORD
+        password.submit()
+        sleep(3)
+        # TODO Handle incorrect user/password
+
+        # Navigate to target page
+        self.DRIVER.get(TARGET)
+        sleep(3)
+        # TODO Handle TARGET not being found (incorrect username)
+
+    def scroll_and_grab(self, speed: float=2.00) -> set:
+        """Scroll to page bottom and return a set of all picture hrefs.
+
+        Args:
+            speed: Seconds to sleep after each scroll (default=2.00).
+        """
+        links = set()  # Automatically filter duplicates
+
+        while True:
+            # Add all currently 'visible' posts to the list
+            path = "//div[@class='Nnq7C weEfm']//descendant::a"
+            for x in self.DRIVER.find_elements_by_xpath(path):
+                links.add(x.get_attribute('href'))
+
+            # Get current page height and scroll to the bottom of the page
+            js_height = "return document.body.scrollHeight"
+            js_scroll = "window.scrollTo(0, document.body.scrollHeight);"
+            height = self.DRIVER.execute_script(js_height)
+            self.DRIVER.execute_script(js_scroll)
+            sleep(speed)  # Allow refreshing
+
+            # Compare height values
+            current = self.DRIVER.execute_script(js_height)
+            if current == height:
+                break
+            else:
+                height = current
+        sleep(1)
+        return links
+
+    def log_out(self, speed: float=3.00) -> None:
+        """Loggout and close the browser.
+
+        Args:
+            speed: Seconds to sleep (refresh) between pages (default=3).
+        """
+        HOME = f"https://www.instagram.com/{self.USER}/"
+        SETTINGS = "//button[@class='_0mzm- dCJp8']"
+        LOG_OUT = "//button[text()='Log Out']"
+
+        # Nav to USER's profile
+        self.DRIVER.get(HOME)
+        sleep(3)
+        # Find settings
+        self.DRIVER.find_element_by_xpath(SETTINGS).click()
+        # Loggout and close browser.
+        self.DRIVER.find_element_by_xpath(LOG_OUT).click()
+        sleep(3)
+        self.DRIVER.quit()
+
+    def test(self) -> str:
+        """Test"""
+        # Find the total number of pictures
+        POSTS = self.DRIVER.find_element_by_class_name("g47SY ")
+        TOTAL = POSTS.get_attribute("textContent")
+        print(f"\nTotal posts: {TOTAL}")
+
+        # TODO Find the total number of 'liked' pictures
+        try:
+            # Scroll to bottom of page and build a list of picture hrefs
+            pics = self.scroll_and_grab()
+            print(f"Pictures found: {len(pics)}")
+        except Exception:
+            print("Something went wrong")
+        finally:
+            self.log_out()
+
+        # TODO Like un-liked pictures
+        # TODO Like each un-liked pictures
 
 
-def loggout(speed: float=3.00) -> None:
-    """Loggout and close the browser.
-
-    Args:
-        speed: Seconds to sleep prior to accessing page 'settings' (default=3).
-    """
-    # Nav to USER's profile
-    DRIVER.get(HOME)
-    sleep(3)
-    # Find settings
-    settings = "//button[@class='_0mzm- dCJp8']"
-    DRIVER.find_element_by_xpath(settings).click()
-    # Loggout and close browser.
-    loggout = "//button[text()='Log Out']"
-    DRIVER.find_element_by_xpath(loggout).click()
-    sleep(3)
-    DRIVER.quit()
-
-
-# ### Open browser, log in, navigate to target. ### #
-DRIVER.get(GRAM)
-sleep(3)
-
-username = DRIVER.find_element_by_name("username")
-password = DRIVER.find_element_by_name("password")
-username.send_keys(USER)
-password.send_keys(PASSWORD)
-password.submit()
-sleep(3)
-# TODO Handle incorrect user/password
-
-DRIVER.get(TARGET)
-sleep(3)
-# TODO Handle TARGET not being found (incorrect username)
-
-# Find the total number of pictures
-_ = DRIVER.find_element_by_class_name("g47SY ")
-total = _.get_attribute("textContent")
-print(f"\nTotal posts: {total}")
-
-# TODO Find the total number of 'liked' pictures
-try:
-    # Scroll to bottom of page and build a list of picture hrefs
-    pics = scroll_and_grab()
-    print(f"Pictures found: {len(pics)}")
-except Exception:
-    print("Something went wrong")
-finally:
-    loggout()
-
-# TODO Like un-liked pictures
-# TODO Like each un-liked pictures
+if __name__ == "__main__":
+    main = BaeFinder()
+    main.log_in()
+    main.test()
+    main.log_out
