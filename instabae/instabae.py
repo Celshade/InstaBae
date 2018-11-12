0b5ad13b3db2b8ec9338ac384c9bbc13d22e592e
Danny 'Celshade' Collins <ggcelshade@gmail.com>
2018-11-12 12:14:19 -0500

    Add Chrome support; add config() + main() to instabae.py

--- instabae/instabae.py
+++ instabae/instabae.py
@@ -3,42 +3,37 @@
 Classes:
     BaeFinder(object): Handle communication with Instagram.
 Functions:
-    # TODO
-Attributes:
-    # TODO
+    config(): Specify the configuration for BaeFinder().
+    main(): Summon BaeFinder().
 """
 from time import sleep
 
 from selenium import webdriver
 
-# Drivers
-GECKO = "../drivers/geckodriver.exe"
-
 
 class BaeFinder(object):
-    """Search Instagram for BAE and like their un-liked pictures.
+    """Search Instagram for Bae and like their un-liked pictures.
 
     Attributes:
-        # TODO
-        TARGET: The username of your target.
+        DRIVER: The specified driver to utilize.
+        BAE: The username of the target bae.
+        USER: The username of the user.
     Public Methods:
         log_in(): Log into Instagram.
-        scroll_and_grab(): Scroll through the page and gather post info.
+        scroll_and_grab(): Scroll through the page and gather posts.
         log_out(): Log out of Instagram and close the browser session.
+        test(): Temporary handler of BaeFinder() methods.
     """
 
-    def __init__(self) -> None:
-        self.BAE = input("Enter the username of bae: ")
-        self.USER = input("Enter your username: ")
-        self._PASSWORD = input("Enter your password: ")
-        self.DRIVER = webdriver.Firefox(executable_path=GECKO)
-        # TODO Add additional driver support for IE and Chrome.
-        # TODO Add function to select driver [browser] of choice
+    def __init__(self, driver: webdriver, bae: str, user: str) -> None:
+        self.DRIVER = driver
+        self.BAE = bae
+        self.USER = user
         # TODO Add 'modes' ('S' for Spectator mode | 'N' for Ninja mode)
 
     def __repr__(self) -> str:
         """Return basic information carried by BaeFinder."""
-        pass
+        return f"\n\nWelcome {self.USER} to InstaBae!\nLocating {self.BAE}..."
 
     def log_in(self, speed: float=3.00) -> None:
         """Open browser, log into Instagram, and navigate to target.
@@ -46,6 +41,8 @@ class BaeFinder(object):
         Args:
             speed: Seconds to sleep (refresh) between pages.
         """
+        # TODO Rearrange to allow password input before browser window opens
+        _PASSWORD = input("Please enter your password: ")
         GRAM = "https://www.instagram.com/accounts/login/?source=auth_switcher"
         TARGET = f"https://www.instagram.com/{self.BAE}/"
 
@@ -57,8 +54,7 @@ class BaeFinder(object):
         username = self.DRIVER.find_element_by_name("username")
         password = self.DRIVER.find_element_by_name("password")
         username.send_keys(self.USER)
-        password.send_keys(self._PASSWORD)
-        del self._PASSWORD
+        password.send_keys(_PASSWORD)
         password.submit()
         sleep(3)
         # TODO Handle incorrect user/password
@@ -99,11 +95,12 @@ class BaeFinder(object):
         return links
 
     def log_out(self, speed: float=3.00) -> None:
-        """Loggout and close the browser.
+        """Log out and close the browser.
 
         Args:
-            speed: Seconds to sleep (refresh) between pages (default=3).
+            speed: Seconds to sleep (refresh) between pages (default=3.00).
         """
+        print("Logging out...")
         HOME = f"https://www.instagram.com/{self.USER}/"
         SETTINGS = "//button[@class='_0mzm- dCJp8']"
         LOG_OUT = "//button[text()='Log Out']"
@@ -117,8 +114,9 @@ class BaeFinder(object):
         self.DRIVER.find_element_by_xpath(LOG_OUT).click()
         sleep(3)
         self.DRIVER.quit()
+        print("Session closed successfully!")
 
-    def test(self) -> str:
+    def test(self) -> None:
         """Test"""
         # Find the total number of pictures
         POSTS = self.DRIVER.find_element_by_class_name("g47SY ")
@@ -139,8 +137,43 @@ class BaeFinder(object):
         # TODO Like each un-liked pictures
 
 
+def config() -> list:
+    """Prompt for user input and return the configuration.
+
+    To be used in conjunction with BaeFinder(), by providing the necessary
+    user information (str) and driver (webdriver()) needed to run the program.
+
+    Returns:
+        A list containing your driver, target username, and username.
+    """
+    # Select a driver
+    user = input("\nEnter your username: ")
+    bae = input("Enter the username of your target: ")
+    driver = None
+
+    while driver is None:
+        _ = input("Enter [F] for FireFox or [C] for Chrome: ")
+        if _.lower() == "f":
+            GECKO = "../drivers/geckodriver.exe"
+            driver = webdriver.Firefox(executable_path=GECKO)
+            break
+        elif _.lower() == "c":
+            CHROME = "../drivers/chromedriver.exe"
+            driver = webdriver.Chrome(exectable_path=CHROME)
+            break
+        else:
+            print("\nNot a compatible browser.\n")
+    return [driver, bae, user]
+
+
+def main() -> None:
+    """Summon BaeFinder."""
+    driver, bae, user = config()
+    session = BaeFinder(driver, bae, user)
+    session.log_in()
+    print(session)
+    session.test()
+
+
 if __name__ == "__main__":
-    main = BaeFinder()
-    main.log_in()
-    main.test()
-    main.log_out
+    main()
