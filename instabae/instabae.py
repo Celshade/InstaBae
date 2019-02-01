6f899278a4597603421770af77b253c9bc765d35
Danny 'Celshade' Collins <ggcelshade@gmail.com>
2019-02-01 12:30:03 -0500

    Remove chromedriver support; clean instabae.py

--- instabae/instabae.py
+++ instabae/instabae.py
@@ -15,14 +15,14 @@ from selenium.webdriver import common as COM
 class BaeFinder(object):
     """Search Instagram for Bae and like their un-liked pictures.
 
-    Parameters defined in the config (tuple) parameter are unpacked into each
+    Parameters defined in the config parameter are unpacked into each
     of BeaFinder's attributes.
 
     Attributes:
-        bae (str): The username of the target bae.
+        bae (str): The username of the target.
         user (str): The username of the user.
         __password (str): The password of the user.
-        MODE (str): The mode in which to run BaeFinder() (default='N').
+        MODE (str): The mode in which to run.
         DRIVER (webdriver): The driver to utilize.
     Public Methods:
         log_in(): Log into Instagram.
@@ -35,7 +35,7 @@ class BaeFinder(object):
         # TODO Add 'modes' ('S' for Spectator mode | 'N' for Ninja mode)
 
     def __repr__(self) -> str:
-        """Return basic information carried by BaeFinder."""
+        """Return session information."""
         return f"""
         ::Session info::
         User: {self.user}
@@ -44,15 +44,14 @@ class BaeFinder(object):
         Browser: FireFox (Geckodriver)
         """
 
-    def log_in(self, refresh: float=3.00) -> None:
+    def log_in(self, wait: float=3.00) -> None:
         """Open browser, log into Instagram, and navigate to target.
 
         Args:
-            refresh: Seconds to sleep between pages (default=3.00).
+            wait: Seconds to sleep between pages (default=3.00).
         """
         GRAM = "https://www.instagram.com/accounts/login/?source=auth_switcher"
         TARGET = f"https://www.instagram.com/{self.bae}/"
-        # Welcome message
         message = f"Welcome to InstaBae {self.user}!"
         wrap = '=' * len(message)
         title = f"\n{wrap}\n{message}\n{wrap}"
@@ -60,7 +59,7 @@ class BaeFinder(object):
         # Open browser -> Instagram
         print(title)
         self.DRIVER.get(GRAM)
-        sleep(refresh)
+        sleep(wait)
 
         # Log in
         print("Logging in...")
@@ -75,12 +74,12 @@ class BaeFinder(object):
         # Navigate to target page
         print(f"Locating {self.bae}...")
         self.DRIVER.get(TARGET)
-        sleep(refresh)
+        sleep(wait)
         # TODO Handle TARGET not being found (incorrect username)
         print("Profile located.")
 
     def scroll_and_grab(self) -> None:
-        """Scroll to page bottom and return a set of all picture hrefs."""
+        """Scroll to page bottom and collect a list of all picture hrefs."""
         POSTS = self.DRIVER.find_element_by_class_name("g47SY ")
         TOTAL = int(POSTS.get_attribute("textContent").replace(",", ""))
         links = []
@@ -91,28 +90,46 @@ class BaeFinder(object):
             # TODO Like un-liked pictures
             # ACTIONS = WEB.ActionChains(self.DRIVER)
             # ESC = COM.keys.Keys.ESCAPE
-            # Add all currently 'visible' posts to the list
-            path = "//div[@class='Nnq7C weEfm']//descendant::a"
+            href_path = "//div[@class='v1Nh3 kIKUG  _bz0w']/child::a"
+            # For zipping href nodes with clickable post nodes
+            click_path = "//div[@class='eLAPa']"
+            href_bundle = self.DRIVER.find_elements_by_xpath(href_path)
+            click_bundle = self.DRIVER.find_elements_by_xpath(click_path)
 
-            for post in self.DRIVER.find_elements_by_xpath(path):
-                href = post.get_attribute("href")
+            # Gather all currently 'visible' posts
+            # for post in self.DRIVER.find_elements_by_xpath(href_path):
+            for post, clicker in zip(href_bundle, click_bundle):
+                href = post.get_attribute("href")  # The full href
+                sub_index = href.index("/p")
+                suffix = href[sub_index:]  # The href suffix
 
-                if href not in links:
-                    # Scroll to element
-                    # TODO click and close elements
+                if suffix not in links:
+                    # Scroll to element and add suffix to links
                     JS_SCROLL_TO = "arguments[0].scrollIntoView();"
                     self.DRIVER.execute_script(JS_SCROLL_TO, post)
-                    links.append(href)
-                    # ACTIONS.click(post).send_keys(ESC).perform()
+                    links.append(suffix)
+                    print(suffix)
+
+                    # TODO click and close elements
+                    # self.DRIVER.find_element_by_tag_name("body")
+                    # ACTIONS.send_keys(COM.keys.Keys.COMMAND + 't').perform()
+                    # self.DRIVER.find_element_by_tag_name("body")
+                    # ACTIONS.send_keys(COM.keys.Keys.COMMAND + 'w').perform()
+
+                    # clicker.click()  # BUG This works, but SLOWLY!
+                    # heart_class = 'dCJp8 afkep coreSpriteHeartOpen _0mzm-'
+                    # heart_path = f"//button[@class={heart_class}]"
+                    # heart = self.DRIVER.find_element_by_xpath(heart_path)
+
             if len(links) == TOTAL:
                 break
         print(f"Gathered posts: {len(links)}")
 
-    def log_out(self, refresh: float=3.00) -> None:
+    def log_out(self, wait: float=3.00) -> None:
         """Log out and close the browser.
 
         Args:
-            refresh: Seconds to sleep between pages (default=3.00).
+            wait: Seconds to sleep between pages (default=3.00).
         """
         print("Logging out...")
         HOME = f"https://www.instagram.com/{self.user}/"
@@ -121,12 +138,12 @@ class BaeFinder(object):
 
         # Nav to USER's profile
         self.DRIVER.get(HOME)
-        sleep(3)
+        sleep(wait)
         # Find settings
         self.DRIVER.find_element_by_xpath(SETTINGS).click()
         # Loggout and close browser.
         self.DRIVER.find_element_by_xpath(LOG_OUT).click()
-        sleep(refresh)
+        sleep(wait)
         self.DRIVER.quit()
         print("Session closed successfully!")
 
@@ -136,9 +153,6 @@ def config() -> tuple:
 
     To be used in conjunction with BaeFinder(), by providing the necessary
     target/user information, run-mode, and driver.
-
-    Returns:
-        A tuple containing the configuration for Baefinder().
     """
     # Configuration prompts
     bae = input("\nEnter the username of your bae: ")
@@ -150,6 +164,7 @@ def config() -> tuple:
     return (bae, user, password, mode, driver)
 
 
+# Testing purposes only
 def main() -> None:
     """Summon BaeFinder."""
     session = BaeFinder(config())
@@ -158,8 +173,6 @@ def main() -> None:
     session.log_in()
     try:
         session.scroll_and_grab()
-    except Exception:
-        print("Something went wrong")
     finally:
         session.log_out()
 
